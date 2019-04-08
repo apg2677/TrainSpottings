cae2e8489710e5bf5b2adca047575fdda4db534d
Amir P <apourrezaei@gmail.com>
2019-04-08 19:07:38 -0400

    Getting input

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
-    
+    $("#btnSubmit").click(HandleSubmit);
 
 });
+
+
+
+function HandleSubmit(event) {
+    event.preventDefault();
+
+    var trainName = $("#TrainName").val().trim();
+    var destination = $("#Destination").val().trim();
+    var frequency = $("#Frequency").val().trim();
+    var nextArrival = $("#NextArrival").val().trim();
+    var minutesAway = $("#MinutesAway").val().trim();
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(frequency);
+    console.log(nextArrival);
+    console.log(minutesAway);
+
+}
